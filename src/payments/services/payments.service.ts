39b225b4a887ae65b34970d18ea066bff860d7f5
tianjos <thiago.brito700@gmail.com>
2025-10-21 12:42:49 -0300

    refact: undo

--- src/payments/services/payments.service.ts
+++ src/payments/services/payments.service.ts
@@ -6,21 +6,15 @@ import { AccountsService } from 'src/users/services/accounts.service';
 import { UsersService } from 'src/users/services/user.service';
 import { CreatePaymentDto } from '../dtos/create-payment.dto';
 import { AuthorizerService } from './authorizer.service';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Payment } from '../repositories/payment.entity';
-import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class PaymentsService {
     constructor(
-        @InjectRepository(Payment)
-        private readonly paymentRepository: Repository<Payment>,
         @InjectQueue('notification')
         private readonly paymentsQueue: Queue,
         private readonly authorizerService: AuthorizerService,
         private readonly usersService: UsersService,
         private readonly accountsService: AccountsService,
-        private readonly dataSource: DataSource,
     ) { }
 
     async transfer(dto: CreatePaymentDto) {
@@ -30,13 +24,6 @@ export class PaymentsService {
         ])
 
         await this.authorizerService.check()
-
-        this.dataSource.transaction(async (trx) => {
-            trx.
-        })
-        await Promise.all([
-
-        ])
 
         await this.accountsService.transfer(new MakeTransferDto(
             payer?.account!, payee?.account!, dto.value)
